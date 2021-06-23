086c852930d340abf30d3a23446bc60a37fa152d
mcduck <jjjack1000@gmail.com>
2021-06-22 20:34:48 -0400

    made slightly better

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,93 +12,92 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        
-        bool goleft = false;
-        bool goright = false;
-        bool jumping = false;
+        bool onGround;
+        bool goLeft = false;
+        bool goRight = false;
+        double xVel, yVel = 0;
+        double xPos, yPos = 0;
 
-        int jumpSpeed = 10;
         int force = 8;
         int score = 0;
         public Form1()
         {
             InitializeComponent();
+            xPos = player.Left;
+            yPos = player.Top;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            player.Top += jumpSpeed;
+            yPos += yVel;
+            xPos += xVel;
+            player.Left = (int)xPos;
+            player.Top = (int)yPos;
+            if (onGround)
+                xVel *= 0.9;
+            yVel += 1;
 
-            if (jumping && force < 0)
-            {
-                jumping = false;
-            }
+            if (goLeft && xVel > -5)
+                xVel -= 1;
+            if (goRight && xVel < 5)
+                xVel += 1;
 
-            if (goleft)
-            {
-                player.Left -= 5;
-            }
+            
 
-            if (goright)
-            {
-                player.Left += 5;
-            }
-
-            if (jumping)
-            {
-                jumpSpeed = -12;
-                force -= 1;
-            }
-            else
-            {
-                jumpSpeed = 12;
-            }
             foreach (Control x in this.Controls)
             {
                 System.Console.WriteLine(x.Tag);
                 if (x is PictureBox && x.Tag == "platform")
                                     {
-                    if (player.Bounds.IntersectsWith(x.Bounds) && !jumping)
+                    if (player.Bounds.IntersectsWith(x.Bounds))
                     {
-                        force = 8;
-                        player.Top = x.Top - player.Height;
+                        onGround = true;
+                        yVel = 0;
+                        yPos = x.Top - player.Height;
+                        player.Top = (int)yPos;
                     }
                 }
             }
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Image imag = player.Image;
+            e.Graphics.DrawImage(imag, new Point(0, 0));
+        }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
-            if (e.KeyCode == Keys.Left)
-            {
-                goleft = true;
-            }
-            if (e.KeyCode == Keys.Right)
-            {
-                System.Console.WriteLine(e.KeyCode);
-                goright = true;
-            }
-            if (e.KeyCode == Keys.Space && !jumping)
-            {
-                jumping = true;
+
+            switch (e.KeyCode) {
+                case Keys.Left:
+                case Keys.A:
+                    goLeft = true;
+                    break;
+                case Keys.Right:
+                case Keys.D:
+                    goRight = true;
+                    break;
+                case Keys.Space:
+                    if (onGround)
+                        yVel = -15;
+                        onGround = false;
+                    break;
             }
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
+            switch (e.KeyCode)
             {
-                goleft = false;
-            }
-
-            if (e.KeyCode == Keys.Right)
-            {
-                goright = false;
-            }
-            if (jumping)
-            {
-                jumping = false;
+                case Keys.Left:
+                case Keys.A:
+                    goLeft = false;
+                    break;
+                case Keys.Right:
+                case Keys.D:
+                    goRight = false;
+                    break;
             }
         }
     }
